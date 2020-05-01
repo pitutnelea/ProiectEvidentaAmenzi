60a1b3a503e5dfbd16d9bfb1f0f3c7b536f7ab29
pitutnelea <55551834+pitutnelea@users.noreply.github.com>
2020-05-01 22:43:23 +0300

    Update Politist.cpp

--- src/Politist.cpp
+++ src/Politist.cpp
@@ -18,6 +18,16 @@ Politist::~Politist()
     //dtor
 }
 
+bool Politist::operator <(Politist& politist)
+{
+    return (this->_numePolitist > politist.GetNumePolitist());
+}
+
+bool Politist::operator <<(Politist& politist)
+{
+    return (this->_codPolitist > politist.GetCodPolitist());
+}
+
 void Politist::ExportPolitist(ostream& out)
 {
     out << _codPolitist << " " << _numePolitist;
