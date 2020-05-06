1abc98927186f7329aa6dc574e5d2ec47c6d7b9d
pitutnelea <55551834+pitutnelea@users.noreply.github.com>
2020-05-06 23:30:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include "Amenzi.h"
 #include "SectieDePolitie.h"
-#include <stdlib.h>
 #include <iostream>
-#include <ios>  // Provides ios_base::failure
 using namespace std;
 
 void Meniu()
@@ -42,11 +40,17 @@ int main()
     while (nrintrodus !=0)
     {
         Meniu();
-        while (true)
+        while(true)
         {
             try
             {
                 nrintrodus = getValidatedInput<int>();
+                if((nrintrodus < 0) || (nrintrodus > 6))
+                {
+                    cout << "Atentie! Trebuie sa introduceti un numar din lista afisata: ";
+                    cin.clear();
+                    continue;
+                }
             }
             catch (exception e)
             {
@@ -105,9 +109,72 @@ int main()
     }
     return 0;
 }
-    //https://www.youtube.com/watch?v=ANS0WYCsIo8
-    //MAXIMUL DINTRUN VECTOR
-    //https://profs.info.uaic.ro/~infogim/2015/lectii/5/59.pdf
-    //SORTED VECTOR
-    //https://stackoverflow.com/questions/46712581/prevent-duplicate-entry-into-vector-c
-    //https://stackoverflow.com/questions/1041620/whats-the-most-efficient-way-to-erase-duplicates-and-sort-a-vector
+
+/*
+CERINTA
+Scrieți un program care să permită ținerea evidenței amenzilor de circulație într-o secție de
+poliție. În acest scop programul va trebui să ofere următoarele funcționalități:
+1. sa afișeze în consolă un meniu de pornire, unde să enumere, opțiunile de utilizare pe care le are
+utilizatorul
+    • opțiunile sunt: „Adaugă agent”, „Șterge agent”, „Adaugă contravenient”, „Afișează amenzi
+agent”, „Afișează amenzi contravenient”, „Afișează situație amenzi” și „Ieșire”
+2. să permită utilizatorului să introducă opțiunea dorită, să valideze inputul și să îl ceară din nou
+dacă inputul nu e valid (adică să corespundă uneia dintre opțiuni)
+3. dacă inputul e valid să șteargă ecranul și să lanseze în execuție codul corespunzător acelei
+opțiuni, afisând un nou text, cu detalii despre opțiunea selectată.
+4. indiferent de opțiunea selectată, noul meniu care se afișează să aibă și opțiunea „Revenire la
+ecranul anterior”. Dacă utilizatorul o selectează programul să reafișeze meniul principal.
+5. la selectarea opțiunii „Adaugă agent”
+    • să afișeze textul: „Introduceți numele agentului”
+    • să permită utilizatorului să introducă numele.
+    • să îi atribuie un cod unic.
+6. la selectarea opțiunii „Șterge agent”
+    • să afișeze agenții deja definiți și codurile lor, după care să afișeze textul: „Selectați numărul
+agentului de șters”. Să permită utilizatorului să introducă codul agentului
+    • să valideze inputul și să îl ceară din nou dacă inputul nu corespunde codului din dreptul unui
+agent
+    • dacă inputul e valid să șteargă din evidență agentul selectat. Să păstreze însă amenzile
+acordate de acesta, mutându-le în contul unui agent virtual, care păstrează evidența
+amenzilor date de agenții ce au părăsit slujba.
+7. la selectarea opțiunii „Adaugă contravenient”
+    • să afișeze textul: „Introduceți numele șoferului”
+    • să permită utilizatorului să introducă numele.
+    • în continuare să afișeze „Selectați categoria:
+        1.bicicletă
+        2.motoreta-motocicletă
+        3.autoturism
+        4.camion
+        5.tractor
+        0.revenire ecran anterior
+    • să permită utilizatorului să introducă o opțiune, să valideze inputul și să îl ceară din nou
+dacă nu e numeric și cuprins între 0 și 5
+    • dacă inputul e valid să afișeze în continuare „Precizati valoarea amenzii”
+    • să permită utilizatorului să o introducă, să valideze inputul și să îl ceară din nou dacă nu e
+numeric și pozitiv
+    • dacă inputul e valid să șteargă ecranul și să afișeze „Selectați agentul constatator” urmat de
+lista agenților existenți și a codurilor lor.
+    • să permită utilizatorului să introducă codul corespunzător agentului, să valideze inputul și
+să îl ceară din nou dacă inputul nu corespunde codului unui agent.
+    • să verifice dacă contravenientul a mai fost amendat în trecut
+    ◦ dacă da, să adauge noua amendă la total amenzi contravenient
+    ◦ dacă nu, să adauge contravenientul în lista contravenienților
+8. la selectarea opțiunii „Afișează amenzi agent”
+    ◦ să afișeze agenții existenți, după care să afișeze textul: „Selectați codul agentului”
+    ◦ să permită utilizatorului să introducă codul agentului, să valideze inputul și să îl ceară din
+nou dacă inputul nu corespunde codului unui agent.
+    ◦ dacă inputul e valid să afișeze toți contravenienții amendați de el, cu sumele încasate de la ei
+ca și totalul amenzilor date de acel agent
+9. la selectarea opțiunii „Afișează amenzi contravenient”
+    ◦ să afișeze contravenienții existenți, după care să afișeze textul: „Selectați contravenientul”
+    ◦ să permită utilizatorului să introducă contravenientul, să valideze inputul și să îl ceară din
+nou dacă inputul nu corespunde unui contravenient.
+    ◦ dacă inputul e valid să afișeze toate amenzile luate de contravenient, precizând la fiecare
+categoria din care face parte, ca și totalul amenzilor primite de acel contravenient.
+10. la selectarea opțiunii „Afișează situație amenzi”
+    ◦ să afișeze fiecare agent și totalul amenzilor sale, ordonați de la cel cu ce mai mare sumă în
+jos.
+    ◦ să afișeze și totalul amenzilor date de toți agenții.
+11. la selectarea opțiunii „Ieșire” programul să se închidă.
+    ◦ programul să salveze datele despre agenți și contravenienți în fișiere dedicate și să le încarce
+la pornire.
+*/
